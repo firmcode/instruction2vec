e8439e8d1dd636f781d70102a026b9c5eeb1bf23
firmcode <firmcode@naver.com>
2019-09-18 21:23:05 -0700

    instruction2vec update

--- core/gen_gensim_model.py
+++ core/gen_gensim_model.py
@@ -1,31 +1,14 @@
 import gensim
 import re
 from gensim.models import Word2Vec
+import instruction2vec as inst2vec
 
-def refine_asmcode(raw_asmcode):
-	raw_asmcode = raw_asmcode.replace("rep ","rep")
-	raw_asmcode = raw_asmcode.replace(".byte","")
-	raw_asmcode = raw_asmcode.replace('DWORD DPTR ', '')
-	raw_asmcode = raw_asmcode.replace('DWORD PTR ', '')
-	raw_asmcode = raw_asmcode.replace('BYTE PTR ', '')
-	raw_asmcode = raw_asmcode.replace('WORD PTR ', '')
-	raw_asmcode = raw_asmcode.replace('ds:', '')
-	raw_asmcode = raw_asmcode.replace("es:","")
-	raw_asmcode = raw_asmcode.replace('stoses:', '')
-	raw_asmcode = raw_asmcode.replace('[', '')
-	raw_asmcode = raw_asmcode.replace(']', '')
-	raw_asmcode = raw_asmcode.replace("- ","-")
-	raw_asmcode = raw_asmcode.replace("+"," ")
-	raw_asmcode = raw_asmcode.replace("-"," -")
-	raw_asmcode = raw_asmcode.replace("*"," ")
 
-	return raw_asmcode
-
-def read_asmcode_corpus(asmcode_corpus_list):
-	asmcode_corpus_list = refine_asmcode(asmcode_corpus_list)
+def read_asmcode_corpus(asmcode_corpus):
+	asmcode_corpus = inst2vec.refine_asmcode(asmcode_corpus)
 	list_instruction = []
 	
-	asmcode_corpus_list_split = asmcode_corpus_list.split('\n')
+	asmcode_corpus_list_split = asmcode_corpus.split('\n')
 
 	for one_instruction in asmcode_corpus_list_split:
 
@@ -36,13 +19,11 @@ def read_asmcode_corpus(asmcode_corpus_list):
 	return list_instruction
 
 
-def gen_instruction2vec_model(asmcode_corpus_list,vectorsize,save_filename):
-	word_list = read_asmcode_corpus(asmcode_corpus_list)
-	print word_list
+def gen_instruction2vec_model(asmcode_corpus,vectorsize,save_filename):
 
-	model = Word2Vec(word_list, size=vectorsize, window=128, min_count=1, workers=32, iter = 10)
-	print model.wv['push'] # just test
-	print model.wv['eax'] # just test
+	word_list = read_asmcode_corpus(asmcode_corpus)
+
+	model = Word2Vec(word_list, size=vectorsize, window=128, min_count=1, workers=4, iter = 10)
 
 	filename=save_filename
 	model.save(filename)
--- example_instruction2vec.py
+++ example_instruction2vec.py
@@ -1,21 +1,26 @@
 import sys
 import core.gen_gensim_model as gen
+import core.instruction2vec as inst2vec
 
-fun_list = """\
+
+test_asmcode = """
 push   ebp
 mov    ebp,esp
 sub    esp,0x18
-mov    eax,0x10
-sub    eax,0x1
-add    eax,0x19f
-mov    ecx,0x10
-mov    edx,0x0
-div    ecx
-imul   eax,eax,0x10
-sub    esp,eax
-mov    eax,esp
-add    eax,0xf
-shr    eax,0x4
-shl    eax,0x4"""
+mov      ebp -0xc ,eax
+call wmemset
+mov      ebp -0x34 ,0x41414141
+mov      ebp + eax * 4 -0x38 ,0x1"""
 
-model = gen.gen_instruction2vec_model(fun_list,10,"test")
+
+fd = open("./sample/asmcode_corpus", "r")
+asmcode_corpus = fd.read()
+
+vectorsize = 5
+
+model = gen.gen_instruction2vec_model(asmcode_corpus,vectorsize,"test_model")
+
+for one_instruction in test_asmcode.split('\n'):
+	print one_instruction
+	vector_of_intruction = inst2vec.instruction2vec(one_instruction,model,vectorsize)
+	print vector_of_intruction
